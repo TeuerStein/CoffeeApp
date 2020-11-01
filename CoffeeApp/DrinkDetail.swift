56c6a4f42f0364581c367df3e76983ec45260aca
TeuerStein <xenkai123456789@gmail.com>
2020-11-01 10:43:37 +0100

    Added a description & order button to DrinkDetail View

--- CoffeeApp/DrinkDetail.swift
+++ CoffeeApp/DrinkDetail.swift
@@ -10,28 +10,60 @@ import SwiftUI
 struct DrinkDetail: View {
     
     var drink: Drink
+    
     var body: some View {
-        ZStack(alignment: .bottom) {
-            Image(drink.imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            
-            Rectangle()
-                .frame(height: 80)
-                .opacity(0.25)
-                .blur(radius: 10)
-            
-            HStack {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text(drink.name)
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
+        List {
+            ZStack(alignment: .bottom) {
+                Image(drink.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Rectangle()
+                    .frame(height: 80)
+                    .opacity(0.25)
+                    .blur(radius: 10)
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text(drink.name)
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                    }
+                        .padding(.leading)
+                        .padding(.bottom)
+                    Spacer()
                 }
-                    .padding(.leading)
-                    .padding(.bottom)
-                Spacer()
+            }
+                .listRowInsets(EdgeInsets())
+            
+            VStack(alignment: .leading) {
+                Text(drink.description)
+                    .foregroundColor(.primary)
+                    .font(.body)
+                    .lineLimit(nil)
+                    .lineSpacing(12)
+                
+                HStack {
+                    Spacer()
+                    OrderButton()
+                    Spacer()
+                }
+                    .padding(.top, 50)
             }
         }
+    }
+}
+
+struct OrderButton: View {
+    var body: some View {
+        Button(action: {}) {
+            Text("Order Now")
+        }
+            .frame(width: 200, height: 50)
+            .foregroundColor(.white)
+            .font(.headline)
+            .background(Color.blue)
+            .cornerRadius(10)
     }
 }
 
